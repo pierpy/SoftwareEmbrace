4051dcffa53f7c90e7255106f59054c091df3b6d
rbruna <ricbruna@ucm.es>
2025-07-25 02:42:01 +0200

    General functions (from the C3N MEEG analysis pipeline)

---
+++ dyads_ciplv_code/script/functions/my_read_event.m
@@ -0,0 +1,36 @@
+function event = my_read_event ( filename, header )
+
+% Gets the file header.
+if nargin < 2
+    header = my_read_header ( filename );
+end
+
+% Tries to use the specific function.
+if ft_filetype ( filename, 'neuromag_fif' )
+    event  = myfiff_read_event ( filename, header );
+    
+elseif ft_filetype ( filename, 'egi_mff' )
+    event  = mymff_read_event ( filename, header );
+    
+elseif ft_filetype ( filename, 'ns_cnt' )
+    event  = mycnt_read_event ( filename, 'header', header );
+    
+elseif ft_filetype ( filename, 'eep_cnt' )
+    event  = myeep_read_event ( filename, header );
+    
+elseif ft_filetype ( filename, 'brainvision_vhdr' ) || ft_filetype ( filename, 'brainvision_eeg' )
+    event  = mybv_read_event ( filename, header );
+    
+elseif ft_filetype ( filename, 'biosemi_bdf' )
+    event  = [];
+    return
+    
+% If no specific function relies on FieldTrip.
+else
+    event  = ft_read_event ( filename );
+end
+
+
+% Sorts the events by sample.
+[ ~, sorting ] = sort ( [ event.sample ] );
+event  = event ( sorting );
---
+++ dyads_ciplv_code/script/functions/myft_path.m
@@ -0,0 +1,27 @@
+function myft_path
+
+% Looks for FieldTrip in the parent folder.
+ft_path = dir ( sprintf ( '%s/fieldtrip*', fileparts ( pwd ) ) );
+ft_path = ft_path ( [ ft_path.isdir ] );
+
+% Search for a version of FiedlTrip in the path.
+if ~isempty ( which ( 'ft_defaults' ) )
+    
+    % Does nothing.
+    
+% Adds, if any version, FieldTrip to the path.
+elseif numel ( ft_path )
+    addpath ( sprintf ( '%s/%s/', fileparts ( pwd ), ft_path ( end ).name ) )
+    
+% Otherwise exits with an error.
+else
+    error ( 'preproc:NoFT', 'No FieldTrip version neither in your path nor in the parent of the current folder.\nThis script cannot continue.' )
+end
+
+% Initializes the FieldTrip setup.
+ft_defaults
+
+% Disables the FT feedback.
+global ft_default;
+ft_default.showcallinfo = 'no';
+ft_default.checkconfig  = 'silent';
